b299d4166c7504c13f5b413cf922bab809c03dc2
rick-vini <rick.vini@gmail.com>
2019-12-18 18:49:11 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -60,7 +60,7 @@ sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/moodle/g' /etc/
 #SSLCertificateKeyFile /etc/apache2/ssl/server.key
 
 
-sed -i /ServerAdmin/a #RewriteEngine On RewriteCond %{HTTPS} !=on RewriteRule ^/?(.*) https://%{SERVER_NAME}/$1 [R,L] /etc/apache2/sites-available/000-default.conf
+sed -i '/ServerAdmin/a #RewriteEngine On RewriteCond %{HTTPS} !=on RewriteRule ^/?(.*) https://%{SERVER_NAME}/$1 [R,L]' /etc/apache2/sites-available/000-default.conf
 
 
 #/etc/init.d/apache2 restart
