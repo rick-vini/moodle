48e202d816ba5d0d8ebfcfd90751c190e46359a1
rick-vini <rick.vini@gmail.com>
2019-12-18 18:57:03 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,40 +33,28 @@ chmod -R 755 /var/www/moodledata
 
 sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/moodle/g' /etc/apache2/sites-available/000-default.conf
 sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/moodle/g' /etc/apache2/sites-available/default-ssl.conf
-# vi /etc/apache2/sites-available/default
-#On about line 4, change DocumentRoot "/var/www/html" to
-
-#DocumentRoot "/var/www/moodle"
-#On about line 10, change <Directory "/var/www/html/"> to
-
-#<Directory "/var/www/moodle/">
-#Around line 17, comment out the line for the default page:
-
-# RedirectMatch ^/$ /apache2-default/
-#You can change other values like ServerAdmin if appropriate. For all changes, you should restart Apache for the new settings to take effect.
 
 
 ######## HTTPS #######
-#mkdir /etc/apache2/ssl
+mkdir /etc/apache2/ssl
 #openssl req -x509 -nodes -days 1095 -newkey rsa:4096 -out /etc/apache2/ssl/server.crt -keyout /etc/apache2/ssl/server.key
 
-#a2enmod rewrite
-#a2enmod ssl
+a2enmod rewrite
+a2enmod ssl
  
-#ln -s /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-enabled/000-default-ssl.conf
+ln -s /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-enabled/000-default-ssl.conf
 #vi /etc/apache2/sites-enabled/000-default-ssl.conf
 
 #SSLCertificateFile    /etc/apache2/ssl/server.crt
 #SSLCertificateKeyFile /etc/apache2/ssl/server.key
 
-
 sed -i '/ServerAdmin/a #RewriteEngine On RewriteCond %{HTTPS} !=on RewriteRule ^/?(.*) https://%{SERVER_NAME}/$1 [R,L]' /etc/apache2/sites-available/000-default.conf
 
-
-#/etc/init.d/apache2 restart
+/etc/init.d/apache2 restart
 
 ############### CRON ###############
 
-# crontab -u www-data -e
+(crontab -u www-data -l 2>/dev/null; echo "*/10 * * * * /usr/bin/php /var/www/moodle/admin/cli/cron.php  >/dev/null") | crontab -u www-data -
 
-# */10 * * * * /usr/bin/php /var/www/moodle/admin/cli/cron.php  >/dev/null
+# crontab -u www-data -e
+# */10 * * * * /usr/bin/php /var/www/moodle/admin/cli/cron.php  > /dev/null
