21dd3c84d5f58f01fbb09a4f91340ce8949e346d
rick-vini <rick.vini@gmail.com>
2019-12-18 20:29:17 -0300

    Update bkp.sh

--- bkp.sh
+++ bkp.sh
@@ -9,21 +9,21 @@ NOW=$(date +"%d-%m-%Y")
 #Faz Backup colocando a data no nome do arquivo
 /usr/bin/mysqldump -u $USERNAME -p$PASSWORD --databases moodle > $DATADIR/moodle.$NOW.sql
 if [ "$?" -ne "0" ]; then
-        echo "failed!"
+        echo "Falha na copia do banco moodle!"
         exit 1
 fi
 echo "Fim mysqldump"
 
 tar -czf $DATADIR/moodle-$NOW.tar.gz /var/www/moodle
 if [ "$?" -ne "0" ]; then
-        echo "failed!"
+        echo "Falha na copia do diretorio moodle!"
         exit 1
 fi
 echo "Fim copia do diretorio moodle"
 
 tar -czf $DATADIR/moodledata-$NOW.tar.gz /var/www/moodledata
 if [ "$?" -ne "0" ]; then
-        echo "failed!"
+        echo "Falha na copia do diretorio moodledata!"
         exit 1
 fi
 echo "Fim copia do diretorio moodledata"
