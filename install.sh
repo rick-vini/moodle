b8479e4a5ba29947b2c0b458d0538e1c33d98961
rick-vini <rick.vini@gmail.com>
2019-12-18 20:18:00 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -76,13 +76,33 @@ RewriteRule ^/?(.*) https://%{SERVER_NAME}/$1 [R,L]' /etc/apache2/sites-availabl
 # Script que realiza o dump do banco do roundcube. Mantem as ultimas 7 copias por seguranca
 # Criado por ricardo
 
-#DATADIR="/home/cpdsrssa/roundcubebackups"
-#USERNAME="dumpuser"
-#PASSWORD="adminuser"
-#NOW=$(date +"%d-%m-%Y")
+DATADIR="/home/adminuser/"
+USERNAME="dumpuser"
+PASSWORD="adminuser"
+NOW=$(date +"%d-%m-%Y")
 
 #Faz Backup colocando a data no nome do arquivo
-#/usr/bin/mysqldump -u $USERNAME -p$PASSWORD --databases moodle > $DATADIR/moodle.$NOW.sql
+/usr/bin/mysqldump -u $USERNAME -p$PASSWORD --databases moodle > $DATADIR/moodle.$NOW.sql
+if [ "$?" -ne "0" ]; then
+        echo "failed!"
+        exit 1
+fi
+echo "Fim mysqldump"
+
+tar -czf $DATADIR/moodle-$NOW.tar.gz /var/www/moodle
+if [ "$?" -ne "0" ]; then
+        echo "failed!"
+        exit 1
+fi
+echo "Fim copia do diretorio moodle"
+
+tar -czf $DATADIR/moodledata-$NOW.tar.gz /var/www/moodledata
+if [ "$?" -ne "0" ]; then
+        echo "failed!"
+        exit 1
+fi
+echo "Fim copia do diretorio moodledata"
+
 
 # Remove arquivos do diretório informado com mais de uma semana (7 dias)
-#find $DATADIR -mtime +6 -exec rm {} \;
+find $DATADIR -mtime +6 -exec rm {} \;
