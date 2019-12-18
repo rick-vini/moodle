fa3028227013515276b6ef4de086dedcbf22186f
rick-vini <rick.vini@gmail.com>
2019-12-18 17:34:40 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 apt-get install apache2 php7.3 mariadb-server php7.3-mysql libapache2-mod-php7.3 php7.3-gd php7.3-curl php7.3-xmlrpc php7.3-intl php7.3-zip php7.3-mbstring
 
 cp -a /etc/php/7.3/apache2/php.ini /etc/php/7.3/apache2/php.ini.original
